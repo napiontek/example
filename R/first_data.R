9c6ae83ab4ba87a1a85353f99e31dc3771ea3a6b
napiontek <jnapiontek@yahoo.de>
2020-12-20 19:16:52 +0100

    initial commit

---
+++ R/first_data.R
@@ -0,0 +1,8 @@
+#' Sample of 100 data points between 1 and 100
+#'
+#' A data set to be used as example or value or weight
+#'
+#' @format numerical vector of length 100
+#'
+#' @source sample(100)
+"x"
---
+++ R/second_data.R
@@ -0,0 +1,8 @@
+#' Sample of 100 datap oints between 1 and 100
+#'
+#' A data set to be used as example or value or weight
+#'
+#' @format numerical vector of length 100
+#'
+#' @source sample(100)
+"y"
